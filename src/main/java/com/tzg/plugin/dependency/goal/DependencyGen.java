8e8fc87fad02849c71594096f20220769d54de4b
zenglin <zengl@tzg.cn>
2017-05-22 19:19:08 +0800

    fix bug

--- src/main/java/com/tzg/plugin/dependency/goal/DependencyGen.java
+++ src/main/java/com/tzg/plugin/dependency/goal/DependencyGen.java
@@ -15,6 +15,7 @@ import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -69,9 +70,19 @@ public class DependencyGen extends AbstractMojo {
             Document  document = reader.read( pomPath );
 
             Element         dependencies   = DependencySupport.getDependenciesElement( document );
-            List< Element > dependencyList = DependencySupport.getDependencyElement( component, dependencies );
+            List< Element > dependencyList = new ArrayList<>();
+
+            if ( dependencies != null ) {
+                dependencyList = DependencySupport.getDependencyElement( component, dependencies );
+            }
 
             if ( dependencyList.size() == 0 ) {
+
+                if ( dependencies == null ) {
+                    Element rootElement = document.getRootElement();
+                    dependencies = rootElement.addElement( "dependencies" );
+                }
+
                 // create <dependency> node
                 Element dependencyElement = dependencies.addElement( "dependency" );
                 dependencyElement.addElement( "groupId" ).addText( "com.tzg" );
